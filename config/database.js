e7adc70ed573d3c7084790a6f9d48ef134e1ac77
Hozifa Elgharbawy <Hozifa.elgharbawy12245533@gmail.com>
2023-05-04 17:33:17 +0300

    finished

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-//mongodb+srv://akalatAdmin:Akalat12@cluster0.m7crdry.mongodb.net/?retryWrites=true&w=majority
+
 const connection = async () => {
   return mongoose.connect(process.env.MONGOOSE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
